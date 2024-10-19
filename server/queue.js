780d33b6d7869905388d7ff6d773c55980a51b9f
Benhur Külzer <benhur.kulzer@brandlovrs.com>
2024-10-18 23:14:26 -0300

    chore: uncommenting some things

--- server/queue.js
+++ server/queue.js
@@ -2,45 +2,45 @@ const fs = require('fs');
 const path = require('path');
 const queueFilePath = path.join(__dirname, './mocks/queue.json');
 
-// Função para ler a fila do arquivo JSON
 function readQueue() {
     if (fs.existsSync(queueFilePath)) {
         const data = fs.readFileSync(queueFilePath, 'utf8');
         return JSON.parse(data);
     }
+
     return [];
 }
 
-// Função para gravar a fila no arquivo JSON
 function writeQueue(queue) {
     fs.writeFileSync(queueFilePath, JSON.stringify(queue, null, 2), 'utf8');
 }
 
-// Função para adicionar um item à fila
 function enqueue(item) {
     const queue = readQueue();
     queue.push(item);
-    writeQueue(queue); // Salva a fila no arquivo
+
+    writeQueue(queue);
+    
     console.log(`Enqueued: ${JSON.stringify(item)}`);
 }
 
-// Função para remover o primeiro item da fila
 function dequeue() {
     const queue = readQueue();
     const item = queue.shift();
-    writeQueue(queue); // Salva a fila no arquivo
+
+    writeQueue(queue);
     console.log(`Dequeued: ${JSON.stringify(item)}`);
+
     return item;
 }
 
-// Função para obter o estado atual da fila
 function getQueue() {
     return readQueue();
 }
 
-// Função para verificar se a fila está vazia
 function isEmpty() {
     const queue = readQueue();
+
     return queue.length === 0;
 }
 
--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const robots = require('./robots.js'); // Importa a classe Robots
-const locations = require('./mocks/locations.json'); // Importa as localizações
-const queue = require('./queue.js'); // Importa a fila
+const robots = require('./robots.js');
+const locations = require('./mocks/locations.json');
+const queue = require('./queue.js');
 
 const app = express();
 app.use(bodyParser.json());
 
-// Função para registrar na fila
 function logQueueItem(robotId, locationId, status) {
     const queueItem = { robotId, locationId, status };
     queue.enqueue(queueItem);
 }
 
-// Envia o robô para a localização
 function sendRobotToLocation(robot, locationId) {
     const location = locations.find(loc => loc.id === locationId);
 
@@ -25,88 +23,81 @@ function sendRobotToLocation(robot, locationId) {
 
     console.log(`${robot.name} is being sent to ${location.name}`);
 
-    // Atualiza o status do robô para "Running" e diminui 10% da bateria
     robots.updateRobotStatus(robot.id, "Running", -10);
 
-    // Registra o envio do robô na fila
     logQueueItem(robot.id, locationId, "Running");
 
     setTimeout(() => {
         console.log(`${robot.name} has arrived at ${location.name}`);
 
-        // Atualiza o status para "Idle" após chegar à localização
         robots.updateRobotStatus(robot.id, "Idle");
 
-        // Se a bateria estiver baixa, coloca o robô para carregar
         if (robot.battery <= 20) {
             robots.updateRobotStatus(robot.id, "Charging");
 
             setTimeout(() => {
-                robots.updateRobotStatus(robot.id, "Idle", 100); // Recarga a bateria completamente
+                robots.updateRobotStatus(robot.id, "Idle", 100);
                 console.log(`${robot.name} is fully charged and Idle`);
-            }, 3600000); // 1 hora para recarregar totalmente
+            }, 3600000);
         }
 
-        // Processa a próxima tarefa na fila
         waitForIdleRobot();
-    }, 60000); // 1 minuto para o robô chegar ao destino
+    }, 60000);
 }
 
-// Aguarda robôs disponíveis
 function waitForIdleRobot() {
     if (queue.isEmpty()) {
         console.log('Queue is empty, no robots to assign.');
+
         return;
     }
 
-    const availableRobot = robots.getAvailableRobot(); // Verifica se há robô disponível
+    const availableRobot = robots.getAvailableRobot();
 
     if (availableRobot) {
-        const nextInQueue = queue.dequeue(); // Remove o próximo item da fila
+        const nextInQueue = queue.dequeue();
+        
         if (nextInQueue) {
             sendRobotToLocation(availableRobot, nextInQueue.locationId);
         }
     } else {
         console.log('No robots available, still waiting.');
-        setTimeout(waitForIdleRobot, 5000); // Verifica novamente em 5 segundos
+        setTimeout(waitForIdleRobot, 5000);
     }
 }
 
-// Endpoint para obter localizações
 app.get('/api/locations', (req, res) => {
-    res.json(locations); // Retorna a lista de localizações
+    res.json(locations);
 });
 
-// Endpoint para obter robôs
 app.get('/api/robots', (req, res) => {
     res.json(robots.readRobots()); 
 });
 
-// Endpoint para enviar um robô para uma localização
 app.post('/api/robots/call', (req, res) => {
     const { locationId } = req.body;
-    const availableRobot = robots.getAvailableRobot(); // Verifica se há robôs disponíveis
+    const availableRobot = robots.getAvailableRobot();
 
     if (availableRobot) {
-        // Se houver robô disponível, envia-o para a localização
         sendRobotToLocation(availableRobot, locationId);
+
         res.status(200).send(`${availableRobot.name} is being sent to location ID ${locationId}.`);
     } else {
-        // Se não houver robô disponível, adiciona à fila
         const queueItem = { locationId };
-        queue.enqueue(queueItem); // Adiciona à fila
+        queue.enqueue(queueItem);
+
         res.status(200).send(`All robots are busy. Request for location ID ${locationId} has been enqueued.`);
-        waitForIdleRobot(); // Função para aguardar robô ficar disponível
+
+        waitForIdleRobot();
     }
 });
 
-// Endpoint para verificar a fila atual
 app.get('/api/queue', (req, res) => {
     const currentQueue = queue.getQueue();
+
     res.json(currentQueue);
 });
 
-// Inicia o servidor
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
